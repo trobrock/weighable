138b580b87844109e2eaa8fb11c1e441636a9356
Andrew Katz <andrewkatz00@gmail.com>
2016-08-22 21:37:59 -0700

    Abbrevation

--- lib/weighable/weight.rb
+++ lib/weighable/weight.rb
@@ -22,7 +22,7 @@ module Weighable
       unit:      nil
     }.freeze
 
-    ABBREVATION_ALIASES = {
+    ABBREVIATION_ALIASES = {
       'g'  => :gram,
       'oz' => :ounce,
       'lb' => :pound,
@@ -88,7 +88,7 @@ module Weighable
 
     def self.parse(string)
       value, unit = string.split(' ')
-      unit = ABBREVATION_ALIASES[unit]
+      unit = ABBREVIATION_ALIASES[unit]
       fail ArgumentError, 'invalid weight' if unit.nil?
       Weight.new(value, unit)
     end
